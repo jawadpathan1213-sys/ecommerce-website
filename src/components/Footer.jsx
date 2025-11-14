f4e6ec5280285e1bc11d0873d82156b5b43f4240
jawadpathan1213-sys <jawadpathan1213@gmail.com>
2025-11-14 18:55:54 +0500

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm text-gra'>
+       <div>
+        <img src={assets.logo} className='mb-5 w-32' alt="" />
+        <p className='w-full sm:1/2 md:w-3/4 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione molestiae ipsum modi, itaque iusto quas dolorum beatae corrupti earum. Facere architecto eos voluptas nisi, possimus minus ab voluptates illo quas!</p>
+       </div>
+       <div >
+        <p className='text-xl font-semibold mb-5'>COMPANY</p>
+        <ul className='text-gray-600 font-medium flex flex-col gap-1'>
+        <li >Home</li>
+        <li >About us</li>
+        <li >Delivery</li>
+        <li>Privacy policy</li>
+        </ul>
+       </div>
+       <div >
+        <p className='text-xl font-semibold mb-5'>GET IN TOUCH</p>
+        <ul className='text-gray-600 font-medium flex flex-col gap-1'>
+        <li>+92 322 9672054</li>
+        <li>jawadpathan1213@gmail.com</li>
+        <li>Instagram </li>
+        </ul>
+       </div>
+    </div>
+    <hr className='text-gray-300'/>
+      <p className='font-semibold text-center py-3'>Copyright 2024@ greatstack.dev - All Right Reserved.</p>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/context/ShopContextProvider.jsx
@@ -0,0 +1,19 @@
+import { products } from "../assets/assets";
+import { ShopContext } from "./ShopContext";
+
+export const ShopContextProvider = (props) => {
+
+    const currency = '$';
+
+    const delivery_fee = 10 ;
+
+    const value = {
+        products , currency , delivery_fee
+    }
+
+    return(
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
